8c98248c4e53eebc353a449f1eb33f59e235df8d
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2024-04-04 16:05:14 +0530

    Delete SDS_Entry_Maven directory

--- SDS_Entry_Maven/src/main/java/com/smartdatasolutions/test/MemberImporter.java
+++
@@ -1,9 +0,0 @@
-package com.smartdatasolutions.test;
-import java.io.File;
-import java.util.List;
-
-public interface MemberImporter {
-
-	public List< Member > importMembers( File inputFile ) throws Exception;
-
-}
